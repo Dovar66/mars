37b430fb19025b7085d899395b5654927dc0d812
贺伟宗 <xiaohe0949@163.com>
2022-04-15 18:14:44 +0800

    改方法签名

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "324c80b7"
+#define MARS_REVISION "a1e4ba3b"
 #define MARS_PATH "master_hwz_test"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-01-14 16:52:03"
+#define MARS_BUILD_TIME "2022-04-15 18:08:46"
 #define MARS_TAG ""
 
 #endif
