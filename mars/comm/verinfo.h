489ade4412826a0d9c28ea86aaa581cbcc1c70d0
贺伟宗 <xiaohe0949@163.com>
2022-06-16 15:43:14 +0800

    zplog 1.0.1

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e8f592b7"
+#define MARS_REVISION "3d10a3fa"
 #define MARS_PATH "master_hwz_test"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-06-16 14:24:45"
+#define MARS_BUILD_TIME "2022-06-16 15:13:01"
 #define MARS_TAG ""
 
 #endif
